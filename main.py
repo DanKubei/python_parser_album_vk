79e595432486a1f960d51311b2ec407667bd2794
DanKubei <tirran45@yandex.ru>
2024-04-24 19:49:31 +0300

    Change script

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-from bs4 import BeautifulSoup
 import time
 import requests
 import datetime
@@ -14,35 +13,29 @@ base_url = base_url[0:-1]
 
 driver = webdriver.Chrome()
 driver.get(url)
-
-container = driver.find_element(by=By.CLASS_NAME, value="AlbumPage__photos")
-#photos = container.find_element(by=By.TAG_NAME, value="div")
+container = driver.find_element(by=By.CLASS_NAME, value="photos_container")
+photos = container.find_element(by=By.TAG_NAME, value="div")
 photo_button = photos.find_element(by=By.TAG_NAME, value="a")
 photo_button.click()
-time.sleep(0.1)
+time.sleep(1)
 close_button = driver.find_element(by=By.CLASS_NAME, value="UnauthActionBox__close")
 if close_button != None:
     close_button.click()
 
 time.sleep(0.5)
 counter = driver.find_element(by=By.CLASS_NAME, value="pv_counter")
+dt = str(datetime.datetime.now()).replace(":", "-").replace(" ","")
 
 while True:
     counts = counter.text.split(" из ")
     if int(counts[0]) == int(counts[1]):
         break
-    driver.execute_script("Photoview.show(false, cur.pvIndex + 1, event);")
-    time.sleep(0.1)
     photo_container = driver.find_element(by=By.ID, value="pv_photo")
     photo = photo_container.find_element(by=By.TAG_NAME, value="img")
-    dt = str(datetime.datetime.now()).replace(":", "-").replace(" ","")
-    print(photo.get_attribute("src"))
     req = requests.get(photo.get_attribute("src"))
+    driver.execute_script("Photoview.show(false, cur.pvIndex + 1, event);")
     with open("Downloads/" + dt + "-" + counts[0] + ".jpg", "wb") as file:
         file.write(req.content)
         file.close()
-    time.sleep(0.1)
-
-time.sleep(1)
 
 driver.quit()
